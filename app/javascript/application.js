fc824a9cb59759df6964ac46198f4a0aba7d7d97
Harrison Broadbent <Harrison Broadbent>
2023-03-27 14:01:57 +1100

    adjust forms and add delete confirmation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import 'datasets/copy_url_to_clipboard'
-
+import "@hotwired/turbo-rails"
 const { createApp } = Vue
 import { directive } from "vue3-click-away";
 
@@ -15,6 +15,18 @@ createApp({
         closeMenu() {
             this.showMenu = false
         },
+    },
+    data() {
+        return {
+            // application menu
+            showMenu: false,
+        }
+    }
+}).mount("#desktop-menu")
+console.log("mounted menu app")
+
+createApp({
+    methods: {
         initializeDatasetForm() {
             console.log(gon.dataset_type)
             return gon.dataset_type
@@ -24,19 +36,48 @@ createApp({
             let selectBox = document.getElementById("dataset_dataset_type")
             this.selectedDataType = selectBox.value
             console.log(this.selectedDataType, this.showFileField, this.showURLField)
-        }
+        },
     },
     data() {
         return {
-            // application menu
-            showMenu: false,
             // edit/create
             selectedDataType: this.initializeDatasetForm(),
         }
     },
-}).mount("#application")
+}).mount("#dataset-form")
+console.log("mounted dataset vue app")
 
-console.log("mounted application vue app")
+
+createApp({
+    data() {
+        return {
+            table_data: gon.table_data,
+            headers: Object.keys(gon.table_data[0]),
+            active_sort_direction: "desc",
+            active_sort_heading: null,
+        }
+    },
+    methods: {
+        sortTableData(key) {
+            console.log(this, this.table_data)
+            // data is an array of "col#" hashes, which we want to sort asc/desc by "key"
+            let td = _.sortBy(this.table_data, key)
+
+            // if we sorted by this col last time, sort the other way (asc/desc)
+            if (this.active_sort_heading !== key) {
+                this.active_sort_heading = key
+                this.active_sort_direction = "desc"
+            } else {
+                this.active_sort_direction === "desc" ? this.active_sort_direction = "asc" : this.active_sort_direction = "desc"
+            }
+
+            this.active_sort_direction === "asc" ? td.reverse() : td
+            this.table_data = td
+        }
+    }
+}).mount("#data-table")
+console.log("mounted table vue")
+
 
 // datasets/show
 // Copies the page URL to the clipboard when btn is clicked
