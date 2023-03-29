4b811c7302eaed0214b450ff97e26ddc7e861ac9
Harrison Broadbent <Harrison Broadbent>
2023-03-29 21:03:06 +1100

    add analytics view

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,6 +2,6 @@ class AnalyticsController < ApplicationController
 
   # GET /analytics or /analytics.json
   def index
-    @views_last_7_days = current_user.total_dataset_views_last_n_days(7)
+    @users_dataset_views = current_user.total_dataset_views_last_n_days(7)
   end
 end
